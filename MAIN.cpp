8185908e950c110106bebdf4aa70fb4d97cf8e51
jayson-mo <jayson95.morales@gmail.com>
2017-04-17 20:16:06 +1000

    Update MAIN.cpp

--- MAIN.cpp
+++ MAIN.cpp
@@ -1,10 +1,21 @@
 //SEMESTER SOFTWARE PROJECT - A SHIPMENT BUSINESS COMPANY
+
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
  usingnamespace std;
  
  int main ()
  {
- body
+ istream masterFile;
+ masterFile.open ("masterdata.txt")
+  
+ istream truckFile;
+ truckFile.open ("truckdata.txt")
+  
+ istream userFile;
+ userFile.open ("userdata.txt")
  
  
  system ("pause");
